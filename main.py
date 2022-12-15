28e467df777faa7656566462078039faeb333826
efeytas <efeytas@gmail.com>
2022-12-16 02:20:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''pip3 install flask'''
 from flask import Flask,render_template
-
+import mysql.connector
 app = Flask('__name__')
 
 
@@ -8,14 +8,33 @@ app = Flask('__name__')
 def home():
     return render_template('home.html')
 
-@app.route('/endpoint')
-def endpoint():
-    return "This is an end point. You can call send or receive requests by calling this end point."
+@app.route('/profile/<int:Number>')
+def endpoint(Number):
+    print(Number)
+    connection = mysql.connector.connect(
+        host = "clubeedatabase.cucgzk7st4ht.eu-central-1.rds.amazonaws.com",
+        user = "admin",
+        password = "admin123",
+        database = "clubeedb"
+    )
+    query = f"SELECT * FROM users WHERE studentnumber = {Number};"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
 
-@app.route('/linker')
-def linker():
-    return render_template('link.html')
+@app.route('/signup')
+def signup():
+
+    connection = mysql.connector.connect(
+        host = "clubeedatabase.cucgzk7st4ht.eu-central-1.rds.amazonaws.com",
+        user = "admin",
+        password = "admin123",
+        database = "clubeedb"
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
     #app.run(debug=True) #can alter host and port number here. Right now the default host is localhost and port is 5000
+    
