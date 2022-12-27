5185a63c9200bb59c2618cf0d574ed6082b2eb54
efeytas <efeytas@gmail.com>
2022-12-27 23:20:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask('__name__')
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return "helo"
 
 """@app.route('/api/register' , methods=['POST'])
 def register():
